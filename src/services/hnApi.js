ff3964fee27e66dae10b7e759efba0a5aa4f983c
Swarnim-Mishra <79535033+Swarnim-Mishra@users.noreply.github.com>
2021-08-29 17:19:19 +0530

    Update hnApi.js

--- src/services/hnApi.js
+++ src/services/hnApi.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 import { selectFields } from '../selectors/selectFields';
 
-export const baseUrl = 'https://hacker-news.firebaseio.com/v0/';
+export const baseUrl = 'https://hacker-news.firebaseio.com/vo/topstories';
 export const newStoriesUrl = `${baseUrl}newstories.json`;
 export const storyUrl = `${baseUrl}item/`;
 
